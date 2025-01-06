f62811555b8096faf070345a5548af9b4f46198a
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2025-01-06 17:53:34 +0200

    Update private_module_main.tf

--- private_module_main.tf
+++ private_module_main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2" {
-  source      = "git::https://github.com/lirshindalman/ec2-module.git"
+  source      = "git::https://github.com/lirshindalman/test_module.git"
   ami_id      = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID
   instance_type = "t2.micro"
   subnet_id   = "subnet-12345678"       # Replace with a valid Subnet ID
