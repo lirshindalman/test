a6a5e278695f6868e89cb4d977ae9686f056ea06
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2026-01-11 15:00:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ provider "aws" {
   access_key = "AKIAIOSFODNN7EXAMPLE"
   secret_key = "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"
 }
+
+resource "local_file" "npmrc" {
+  content  = "no"
+  filename = ".npmrc"
+}
